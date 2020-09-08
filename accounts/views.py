81eacea10d7330ad87a3f8a04645ba11ed13a146
Tawkir Talukder <ubiquitouss@yahoo.com>
2020-09-08 21:17:16 +0600

    dynamic url addes

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
 
 def home(request):
-    return render(request,'accounts/dashboard.html')
+    orders= Order.objects.all()
+    total_orders=orders.count()
+    customers=Customer.objects.all()
+    total_customers =customers.count()
+    delivered=orders.filter(status='Delivered').count()
+    pending=orders.filter(status='Pending').count()
+
+    context={'customers':customers, 'orders':orders,'total_orders':total_orders,'total_customers':total_customers,
+            'delivered':delivered,'pending':pending}
+    return render(request,'accounts/dashboard.html',context)
 
 def products(request):
-    return render(request,'accounts/products.html')
+    products=Product.objects.all()
+    return render(request,'accounts/products.html',{'products':products})
 
-def customer(request):
-    return render(request,'accounts/customer.html')
+def customer(request,pk_test):
+    customer=Customer.objects.get(id=pk_test)
+    
+    orders=customer.order_set.all()
+    order_count=orders.count()
+    context={'customer':customer,'orders':orders,'order_count':order_count}
+    return render(request,'accounts/customer.html',context)
